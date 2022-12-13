bd43d6d562600b3aea7e11e6bd69ad4be7f6996e
Gose <niabc70@gmail.com>
2022-12-13 17:03:07 +0900

    css 추가

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
+import styles from "./TodoList.module.css";
 
-function TodoList() {
+function TodoList({ filter }) {
   const [todos, setTodos] = useState([
     { id: "123", text: "장보기", status: "active" },
     { id: "124", text: "공부하기", status: "active" },
@@ -14,10 +15,11 @@ function TodoList() {
   const handleDelete = (deleted) =>
     setTodos(todos.filter((t) => t.id !== deleted.id));
 
+  const filtered = getFilteredItems(todos, filter);
   return (
-    <section>
-      <ul>
-        {todos.map((item) => (
+    <section className={styles.container}>
+      <ul className={styles.list}>
+        {filtered.map((item) => (
           <Todo
             key={item.id}
             todo={item}
@@ -32,3 +34,10 @@ function TodoList() {
 }
 
 export default TodoList;
+
+function getFilteredItems(todos, filter) {
+  if (filter === "all") {
+    return todos;
+  }
+  return todos.filter((todo) => todo.status === filter);
+}
